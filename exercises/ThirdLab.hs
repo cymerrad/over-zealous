2bf2036feb0f6f95fc5849425ea38251c6121f1e
Radosław Cymer <cymerrad@gmail.com>
2018-04-08 12:04:01 +0200

    This always comes as a suprise when code compiles

--- exercises/ThirdLab.hs
+++ exercises/ThirdLab.hs
@@ -1,8 +1,9 @@
 module ThirdLab where
 
 import FirstLab ( flatten )
+import qualified Data.Map as Map
 
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Eq, Ord, Show)
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Eq, Ord) --, Show)
 
 safeGet :: [a] -> Int -> Maybe a
 safeGet l i | length l <= i = Nothing | otherwise = Just (l!!i)
@@ -31,15 +32,25 @@ listToTree (x:xs) =
         Nothing -> Empty
         Just v -> Node v (listToTree (leftBranch xs)) (listToTree (rightBranch xs))
 
--- depth :: Tree a -> Int
--- depth t =
---     case t of
---         Empty -> 0
---         Node _ t1 t2 -> max (depth t1) (depth t2) + 1
+depth :: Tree a -> Int
+depth t =
+    case t of
+        Empty -> 0
+        Node _ t1 t2 -> max (depth t1) (depth t2) + 1
 
--- instance Show a => Show (Tree a) where
---     show t = case t of
+instance Show a => Show (Tree a) where
+    show t = show (showRec t (Map.empty) 0)
 
- 
+showRec :: Tree a -> Map.Map Int [a] -> Int -> Map.Map Int [a]
+showRec t acc d =
+    case t of
+        Empty -> acc
+        Node v left right ->
+            let
+                mLeft = showRec left acc (d+1)
+                -- mRight = showRec right acc (d+1)
+            in
+                Map.insertWith (++) d [v] (showRec right mLeft (d+1))
+
 --  instance Eq a => Eq (Tree a) where
 --     t1 == t2 = ...
